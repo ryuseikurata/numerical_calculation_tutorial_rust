3156b8743abc3cf6837394bdd9480976e157ab6f
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-06-19 12:58:30 +0900

    feat: add ndarray

--- ndarray/src/main.rs
+++ ndarray/src/main.rs
@@ -1,9 +1,10 @@
 use ndarray::array;
 
 fn main() {
-    let a = array![[1., 2., 3.], [4., 5., 6.]];
+    let a = array![[1.0, 2.00, 3.00], [4.0, 5.0, 6.0]];
     assert_eq!(a.ndim(), 2);
     assert_eq!(a.len(), 6);
     assert_eq!(a.shape(), [2,3]);
     assert_eq!(a.is_empty(), false);   
+    println!("{:?}", a);
 }
